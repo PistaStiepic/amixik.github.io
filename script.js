3c8b758caa248b7e57abb57741bb12d5c727eb28
amix <pistastiepic@gmail.com>
2024-02-28 21:54:19 +0100

    .

--- script.js
+++ script.js
@@ -18,3 +18,7 @@ btnPopup.addEventListener("click", ()=> {
 iconClose.addEventListener("click", ()=> {
     wrapper.classList.remove("active-popup");
 });
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
