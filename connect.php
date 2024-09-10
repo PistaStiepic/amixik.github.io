ddf9c31c54b8a7b5269b1ec71bb79fc720f1c684
amix <pistastiepic@gmail.com>
2024-09-10 09:23:50 +0200

    sdaasdasdasd

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $conn = new mysqli("localhost","root","","test");
 if($conn->connect_error){
     die("Connection failed : " .$conn->connect_error);
 }else{
-    $stmt = $conn->prepare("insert into registration(username , email , password )
+    $stmt = $conn->prepare("insert into registration (username , email , password )
     values(? ,?, ?)");
     $stmt->bind_param("sss",$uername , $email ,$password );
     $stmt->execute();
